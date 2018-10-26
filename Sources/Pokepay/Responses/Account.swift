6a9592fba6c370e9a4ebf5ec1d5dba311f150005
Eitaro Fukamachi <e.arrows@gmail.com>
2018-10-26 18:21:23 +0900

    Add Account.{moneyBalance,pointBalance,nearestExpiresAt}.

--- Sources/Pokepay/Responses/Account.swift
+++ Sources/Pokepay/Responses/Account.swift
@@ -4,14 +4,20 @@ public struct Account: Codable {
     public let id: String
     public let name: String
     public let balance: Double
+    public let moneyBalance: Double
+    public let pointBalance: Double
     public let isSuspended: Bool
     public let privateMoney: PrivateMoney
+    public let nearestExpiresAt: String?
 
     private enum CodingKeys: String, CodingKey {
         case id
         case name
         case balance
+        case moneyBalance = "money_balance"
+        case pointBalance = "point_balance"
         case isSuspended = "is_suspended"
         case privateMoney = "private_money"
+        case nearestExpiresAt = "nearest_expires_at"
     }
 }
