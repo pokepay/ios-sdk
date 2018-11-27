7bccc05f43e5d10105b16621e3f8c2b151da5053
Eitaro Fukamachi <e.arrows@gmail.com>
2018-11-27 12:15:40 +0900

    Add 'private_money' to MessageAttachment.

--- Sources/Pokepay/Responses/MessageAttachment.swift
+++ Sources/Pokepay/Responses/MessageAttachment.swift
@@ -3,12 +3,14 @@ import Foundation
 public struct MessageAttachment: Codable {
     public let moneyAmount: Double
     public let pointAmount: Double
+    public let privateMoney: PrivateMoney
     public let isReceived: Bool
     public let expiresAt: Date
 
     private enum CodingKeys: String, CodingKey {
         case moneyAmount = "money_amount"
         case pointAmount = "point_amount"
+        case privateMoney = "private_money"
         case isReceived = "is_received"
         case expiresAt = "expires_at"
     }
