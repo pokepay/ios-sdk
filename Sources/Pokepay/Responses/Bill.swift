ab583feb6ea10f4c3f1df0a595876e4098b9f0b3
smihica <smihica@gmail.com>
2019-07-04 11:35:32 +0900

    Added missing properties

--- Sources/Pokepay/Responses/Bill.swift
+++ Sources/Pokepay/Responses/Bill.swift
@@ -5,4 +5,23 @@ public struct Bill: Codable {
     public let amount: Double?
     public let description: String
     public let user: User
+    public let privateMoney: PrivateMoney
+    public let isOnetime: Bool
+    public let isDisabled: Bool
+    public let token: String
+    public let minAmount: Double?
+    public let maxAmount: Double?
+
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case amount
+        case description
+        case user
+        case privateMoney = "private_money"
+        case isOnetime = "is_onetime"
+        case isDisabled = "is_disabled"
+        case token
+        case minAmount = "min_amount"
+        case maxAmount = "max_amount"
+    }
 }
--- Sources/Pokepay/Responses/Check.swift
+++ Sources/Pokepay/Responses/Check.swift
@@ -3,6 +3,27 @@ import Foundation
 public struct Check: Codable {
     public let id: String
     public let amount: Double
+    public let moneyAmount: Double
+    public let pointAmount: Double
     public let description: String
     public let user: User
+    public let privateMoney: PrivateMoney
+    public let isOnetime: Bool
+    public let isDisabled: Bool
+    public let expiresAt: Date
+    public let token: String
+
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case amount
+        case moneyAmount = "money_amount"
+        case pointAmount = "point_amount"
+        case description
+        case user
+        case privateMoney = "private_money"
+        case isOnetime = "is_onetime"
+        case isDisabled = "is_disabled"
+        case expiresAt = "expires_at"
+        case token
+    }
 }
