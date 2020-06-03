d4a10eecb44428d660ab8f3811d3175c09ce90c6
smihica <smihica@gmail.com>
2020-06-04 00:28:12 +0900

    Enhanced OAuthAPIError structure

--- Sources/Pokepay/OAuthAPI/OAuthAPIError.swift
+++ Sources/Pokepay/OAuthAPI/OAuthAPIError.swift
@@ -1,7 +1,6 @@
 import Foundation
 
-enum OAuthAPIError: Error {
-    case invalidJSON(Swift.Error)
+public enum OAuthAPIErrorType {
     case unknown
     case invalidRequest
     case invalidClient
@@ -12,21 +11,43 @@ enum OAuthAPIError: Error {
     case unrecognizedClient
     case invalidRedirectUri
     case unsupportedResponseType
+    init(error: String) {
+        switch error {
+        case "invalid_request": self = .invalidRequest
+        case "invalid_client": self = .invalidClient
+        case "invalid_grant": self = .invalidGrant
+        case "unauthorized_client": self = .unauthorizedClient
+        case "unsupported_grant_type": self  = .unsupportedGrantType
+        case "invalid_scope": self = .invalidScope
+        case "unrecognized_client": self = .unrecognizedClient
+        case "invalid_redirect_uri": self = .invalidRedirectUri
+        case "unsupported_response_type": self = .unsupportedResponseType
+        default: self = .unknown
+        }
+    }
+}
 
-    init(object: Data) {
+public enum OAuthAPIError: Error {
+    case invalidJSON(Swift.Error)
+    case unknownError(Int, OAuthError, OAuthAPIErrorType)
+    case clientError(Int, OAuthError, OAuthAPIErrorType)
+    case serverError(Int, OAuthError, OAuthAPIErrorType)
+
+    init(statusCode: Int? = nil, object: Data) {
         do {
             let error = try JSONDecoder().decode(OAuthError.self, from: object)
-            switch error.error {
-            case "invalid_request": self = .invalidRequest
-            case "invalid_client": self = .invalidClient
-            case "invalid_grant": self = .invalidGrant
-            case "unauthorized_client": self = .unauthorizedClient
-            case "unsupported_grant_type": self  = .unsupportedGrantType
-            case "invalid_scope": self = .invalidScope
-            case "unrecognized_client": self = .unrecognizedClient
-            case "invalid_redirect_uri": self = .invalidRedirectUri
-            case "unsupported_response_type": self = .unsupportedResponseType
-            default: self = .unknown
+            guard let code = statusCode else {
+                self = .unknownError(0, error, OAuthAPIErrorType(error: error.error))
+                return
+            }
+            if ((400..<500).contains(code)) {
+                self = .clientError(code, error, OAuthAPIErrorType(error: error.error))
+            }
+            else if ((500..<600).contains(code)) {
+                self = .serverError(code, error, OAuthAPIErrorType(error: error.error))
+            }
+            else {
+                self = .unknownError(code, error, OAuthAPIErrorType(error: error.error))
             }
         } catch let decodeError {
             self = .invalidJSON(decodeError)
