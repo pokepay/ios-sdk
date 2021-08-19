511ba052bb9fd3433fcf2e103ba88768fc918b03
Ann <ann.lin@coilinc.jp>
2021-08-19 11:49:18 +0800

    make some variable to be nullable

--- Sources/Pokepay/Responses/PaginatedCoupons.swift
+++ Sources/Pokepay/Responses/PaginatedCoupons.swift
@@ -3,8 +3,8 @@ import Foundation
 public struct PaginatedCoupons:Codable{
     public let perPage:Int
     public let count:Int
-    public let next:String
-    public let prev:String
+    public let next:String?
+    public let prev:String?
     public let items:[Coupon]
     
     private enum CodingKeys: String, CodingKey {
