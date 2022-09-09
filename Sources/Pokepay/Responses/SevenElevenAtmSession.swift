d218f54c75d5df656dc4620525235299cf9cc1ec
zwewaiyanhtet <zwyhmdy@gmail.com>
2022-09-09 16:57:18 +0900

    Update SevenElevenAtmSession Response and added API test logs

--- Sources/Pokepay/Responses/SevenElevenAtmSession.swift
+++ Sources/Pokepay/Responses/SevenElevenAtmSession.swift
@@ -2,10 +2,12 @@ public struct SevenElevenAtmSession: Codable {
     public let qrInfo: String
     public let amount: Double
     public let account: Account
+    public let transaction: UserTransaction?
 
     private enum CodingKeys: String, CodingKey {
         case qrInfo = "qr_info"
         case amount
         case account
+        case transaction
     }
 }
