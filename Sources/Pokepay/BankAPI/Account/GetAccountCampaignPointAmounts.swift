abb96488b1c26a95a733d039115b4596cfba8859
DanRidh <danialridh1@gmail.com>
2023-10-03 17:04:56 +0900

    GetAccountCampaignPointAmounts endpoint

---
+++ Sources/Pokepay/BankAPI/Account/GetAccountCampaignPointAmounts.swift
@@ -0,0 +1,23 @@
+import APIKit
+import Foundation
+
+public extension BankAPI.Account {
+    struct GetAccountCampaignPointAmounts: BankRequest {
+        public let accountId: String
+        public let campaignId: String
+        public typealias Response = AccountCampaignPointAmounts
+
+        public init(accountId: String, campaignId: String) {
+            self.accountId = accountId
+            self.campaignId = campaignId
+        }
+
+        public var method: HTTPMethod {
+            return .post
+        }
+
+        public var path: String {
+            return "/accounts/\(accountId)/campaigns/\(campaignId)/point-amounts"
+        }
+    }
+}
