db6f46e702adbddb83f09db502c29d7ad9927486
DanRidh <danialridh1@gmail.com>
2023-10-05 13:41:12 +0900

    add kana to createBankPay params and paytreeCustomerNumber to response

--- Sources/Pokepay/Responses/BankPayRedirectUrl.swift
+++ Sources/Pokepay/Responses/BankPayRedirectUrl.swift
@@ -2,9 +2,11 @@ import Foundation
 
 public struct BankPayRedirectUrl: Codable {
     public let redirectUrl: String
+    public let paytreeCustomerNumber: String?
 
     private enum CodingKeys: String, CodingKey {
         case redirectUrl
+        case paytreeCustomerNumber
     }
 }
 
