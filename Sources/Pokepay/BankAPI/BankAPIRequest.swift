ce2fa52693cbf8684dd4601a9b3ffdb8245680c5
cxxxr <g23tlm@gmail.com>
2024-07-25 21:35:49 +0900

    add text/html support

--- Sources/Pokepay/BankAPI/BankAPIRequest.swift
+++ Sources/Pokepay/BankAPI/BankAPIRequest.swift
@@ -4,11 +4,16 @@ import APIKit
 let DEFAULT_API_BASE_URL = "https://api-sandbox.pokepay.jp"
 
 public protocol BankRequest: Request {
+    var responseContentType: String { get }
 }
 
 public extension BankRequest {
     var baseURL: URL {
         return URL(string: DEFAULT_API_BASE_URL)!
+    }
+
+    var responseContentType: String {
+        return "application/json"
     }
 }
 
@@ -23,17 +28,33 @@ public extension BankRequest {
 
 extension BankRequest where Response: Decodable {
     public var dataParser: DataParser {
-        return DecodableDataParser()
+        switch responseContentType {
+        case "text/html":
+            return HTMLDataParser()
+        default:
+            return DecodableDataParser()
+        }
     }
 
     public func response(from object: Any, urlResponse: HTTPURLResponse) throws -> Response {
         guard let data = object as? Data else {
             throw BankAPIError(statusCode: urlResponse.statusCode, object: Data())
         }
+
         guard data.count != 0 else {
             let emptyJson = "{}"
             return try BankAPIJSONDecoder().decode(Response.self, from: emptyJson.data(using: .utf8)!)
         }
-        return try BankAPIJSONDecoder().decode(Response.self, from: data)
+
+        switch responseContentType {
+        case "text/html":
+            if let htmlString = String(data: data, encoding: .utf8) as? Response {
+                return htmlString
+            } else {
+                throw BankAPIError(statusCode: urlResponse.statusCode, object: data)
+            }
+        default:
+            return try BankAPIJSONDecoder().decode(Response.self, from: data)
+        }
     }
 }
