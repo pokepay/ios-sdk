819276fc0b4d21e6735a2b998ee1a5d098021a36
zwewaiyanhtet <zwyhmdy@gmail.com>
2024-07-30 12:25:46 +0900

    Updated dependencies to work with xcode 15

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/ishkawa/APIKit", from: "4.0.0"),
-        .package(url: "https://github.com/antitypical/Result.git", from: "4.0.0"),
+        .package(url: "https://github.com/ishkawa/APIKit", from: "5.0.0"),
+        .package(url: "https://github.com/antitypical/Result.git", from: "5.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
