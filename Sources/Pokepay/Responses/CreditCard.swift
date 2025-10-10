bcfced812037efe07ce9ba7d446875e80db62135
masatoi <satoshi.imai@gmail.com>
2025-10-10 10:38:18 +0900

    release-2025-10-09

--- Sources/Pokepay/Responses/CreditCard.swift
+++ Sources/Pokepay/Responses/CreditCard.swift
@@ -5,7 +5,7 @@ public struct CreditCard: Codable {
     public let cardNumber: String;
     public let registeredAt: String;
 
-    private enum CodingKeys: String, CodingKey {
+    private enum CodingKeys: String, CodingKeys {
         case cardNumber = "card_number"
         case registeredAt = "registered_at"
     }
