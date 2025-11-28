45cb78b3d3e433543dde4a611bdfe773d7e37c9c
DanRidh <danialridh1@gmail.com>
2025-11-28 13:44:35 +0900

    add test environments to ios-sdk

--- Sources/Pokepay/Env.swift
+++ Sources/Pokepay/Env.swift
@@ -3,4 +3,8 @@ public enum Env: String {
     case sandbox = "sandbox"
     case development = "dev"
     case qa = "qa"
+    case topic1 = "topic1"
+    case topic2 = "topic2"
+    case topic3 = "topic3"
+    case loadtest = "loadtest"
 }
