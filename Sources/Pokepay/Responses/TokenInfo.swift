28b5b73e5fff7a677c53cc1837ba94db09fe4c87
pokepay-ci <dev@pocket-change.jp>
2026-07-23 06:23:19 +0000

    Generate ios-sdk by pokepay-server generate-mobile-sdk-pr.yml

--- Sources/Pokepay/Responses/TokenInfo.swift
+++
@@ -1,7 +0,0 @@
-public enum TokenInfo {
-    case cashtray (String)
-    case bill (BankAPI.Bill.Get.Response)
-    case check (BankAPI.Check.Get.Response)
-    case cpm (BankAPI.CpmToken.Get.Response)
-    case pokeregi (String)
-}
--- Sources/Pokepay/Responses/UserTagGroup.swift
+++ Sources/Pokepay/Responses/UserTagGroup.swift
@@ -4,9 +4,11 @@ import Foundation
 public struct UserTagGroup: Codable {
     public let groupName: String
     public let id: String
+    public let hasSubgroups: Bool
 
     private enum CodingKeys: String, CodingKey {
         case groupName = "group_name"
         case id = "id"
+        case hasSubgroups = "has_subgroups"
     }
 }
